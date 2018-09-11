08f01c61ab96f8bb42ec61e0c5e0e79bfc00a65a
Luis gerardo <meroser.gs@gmail.com>
2018-09-10 20:29:55 -0500

    Initial commit

---
+++ app/src/main/java/desarrollojhlibreros/com/teoria2botones/ActivityImageButton.java
@@ -0,0 +1,34 @@
+package desarrollojhlibreros.com.teoria2botones;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class ActivityImageButton extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton jib1;
+    ToggleButton jtb1;
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_main);
+        jib1 = (ImageButton) findViewById(R.id.xib1); jib1.setOnClickListener(this);// 3
+        jtb1 = (ToggleButton)findViewById(R.id.xtb1); jtb1.setOnClickListener(this);
+    }
+
+    public void onClick(View v) { // 4
+        if(v.getId()==R.id.xib1) // 5
+            showToastMessage("Botón: ImageButton");
+        else {
+            if (jtb1.isChecked()) showToastMessage("Botón: ToggleButton en ON");
+            else showToastMessage("Botón: ToggleButton en OFF");
+        }
+    }
+
+    private void showToastMessage(String s){
+        Toast t = Toast.makeText(this, s, Toast.LENGTH_SHORT);
+    }
+
+}
